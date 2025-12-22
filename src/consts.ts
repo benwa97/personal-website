40cf857a7f88bcbb526ffdc1b0fde8834811b5d2
Ben Wawczak <wawczakben@gmail.com>
2025-12-21 22:01:36 -0600

    personalized initial template

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'benwa';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'hello world 👋🏼 I am Ben';
